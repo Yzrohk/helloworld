31768285e6b1e075ecaad978ab1f2cd08840f923
Yzrohk <mdlol2011@hotmail.fr>
2023-01-15 22:50:02 +0100

    finition site

--- assets/js/citation.js
+++ assets/js/citation.js
@@ -4,17 +4,29 @@ var citationTexte = document.querySelector(".citation__texte");
 var citationAuteur = document.querySelector(".citation__auteur");
 
 var tabTexte = ["L\'imagination est plus importante que le savoir.",
-    "Un problème sans solution est un problème mal posé",
-    "Qui veut la paix prépare la guerre",
-    "Un gagnant est un rêveur",
-    "Quand on veut on peut, quand on peut on doit",
-    "La vie est un mystère qu\'il faut vivre, et non un problème à résoudre"];
+    "Un problème sans solution est un problème mal posé.",
+    "Qui veut la paix prépare la guerre.",
+    "Un gagnant est un rêveur.",
+    "Quand on veut on peut, quand on peut on doit.",
+    "La vie est un mystère qu\'il faut vivre, et non un problème à résoudre.",
+    "Dans la vie on ne fait pas ce que l\'on veut mais on est responsable de ce que l\'on est.",
+    "L\'éducation, c\'est la famille qui la donne ; l\'instruction, c\'est l\'Etat qui la doit.",
+    "Je crois ce que je dis, je fais ce que je crois.",
+    "On parle toujours mal quand on n\'a rien à dire.",
+    "Plus l\'espérance est grande, plus la déception est violente.",
+    "Ce n\'est pas le doute, c\'est la certitude qui rend fou."];
 var tabAuteur = ["Albert Einstein",
     "Albert Einstein",
     "Jules César",
     "Nelson Mandela",
     "Napoléon Bonaparte",
-    "Gandhi"];
+    "Gandhi",
+    "Jean-Paul Sartre",
+    "Victor Hugo",
+    "Victor Hugo",
+    "Voltaire",
+    "Franz-Olivier Giesbert",
+    "Friedrich Nietzsche"];
 
 var nbDefault = getRandomIntInclusive(0, tabTexte.length);
 citationAuteur.innerHTML = tabAuteur[nbDefault];
--- assets/js/darkmode.js
+++ assets/js/darkmode.js
@@ -2,9 +2,13 @@
 
 var btnDarkmode = document.querySelector(".btn__darkmode");
 var listeSvgDarkmode = document.querySelectorAll(".svg__darkmode");
-var main = document.querySelector(".main");
+/* var main = document.querySelector(".main");
 var header = document.querySelector(".header");
 var footer = document.querySelector(".footer");
+var illuFTP = document.querySelector(".illu__ftp");
+var illuScript = document.querySelector(".illu__script");
+var illustration__big = document.querySelectorAll(".illustration__big");
+var menuListe = document.querySelector(".menu__liste"); */
 
 btnDarkmode.addEventListener("click", applyDarkmode);
 
@@ -24,9 +28,13 @@ function applyDarkmode() {
         }
     });
     document.body.classList.toggle("darkmode");
-    main.classList.toggle("darkmode");
+    /* main.classList.toggle("darkmode");
     header.classList.toggle("darkmode");
     footer.classList.toggle("darkmode");
+    menuListe.classList.toggle("darkmode");
+    illustration__big.forEach(img => {
+        img.classList.toggle("darkmode");
+    }) */
 
     if (document.body.classList.contains("darkmode")) {
         localStorage.setItem("darkmode", "nuit");
